4025f628a593684ba5cdcd8ec23432b8058eb62d
Cameron Brilliant <cbrilliant20@gmail.com>
2021-08-21 11:30:04 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        red: "#A5410F",
+        darkGray: "#506269",
+        lightGray: "#848484",
+        gold: "#D5963D",
+        green: "#131E07",
+        offWhite: "efddd4",
+      },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+    fontFamily: {
+      Montserrat: ["Montserrat, sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
